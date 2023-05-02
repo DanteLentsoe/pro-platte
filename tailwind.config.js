9841a517481d69aba832054ef6b6562c90c584cf
Dante Lentsoe <dante@nml.co.za>
2023-05-02 19:22:58 +0200

    refactor: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        greenSpecial80: "#c5dfd6",
-        whiteTheme80: "#fcfbf4",
-        blackTextMain: "#0e0e0f",
-      },
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                greenSpecial80: "#c5dfd6",
+                greenSpecial40: "#dae8df",
+                greenSpecial20: "#e9f2e8",
+                whiteTheme80: "#fcfbf4",
+                blackTextMain: "#0e0e0f"
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 };
