4fd94d3b0f6ed5a67a4a7fc243de8c1139deefaa
Dante Lentsoe <dante@nml.co.za>
2023-05-02 19:23:35 +0200

    feat: add react error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import "@/styles/index.css";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import '@/styles/index.css'
+import ColorContextProvider from '@/context/ColorContext'
+import { ErrorBoundary } from 'react-error-boundary'
+
+import { useErrorBoundary } from 'react-error-boundary'
+
+function ErrorFallback({ error }) {
+  const { resetBoundary } = useErrorBoundary()
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+      <button onClick={resetBoundary}>Try again</button>
+    </div>
+  )
+}
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary fallback={<ErrorFallback error={undefined} />}>
+      <ColorContextProvider>
+        <Component {...pageProps} />
+      </ColorContextProvider>
+    </ErrorBoundary>
+  )
 }
